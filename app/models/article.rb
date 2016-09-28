20649dc6b01e13273c871279fbb357bd930fa944
DanielJRaine <ley.danielj@gmail.com>
2016-09-28 13:45:35 -0400

    Add full association tests for model Article
    Full set of tests for associations for the Article model.  Includes relationship with comments which has yet to be created.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ActiveRecord::Base
-
+  validates :content, presence: true
+  
+  validates :title, presence: true
+  
+  has_many :comments, inverse_of: :article, dependent: :destroy
 end
